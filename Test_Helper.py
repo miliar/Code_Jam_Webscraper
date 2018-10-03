01715e1facd38ab80cd0c337085cd304b440f1f9
Rashid <miliar1732@gmail.com>
2018-10-03 12:35:44 -0400

    Added .py versions for importing

---
+++ Test_Helper.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import Helper
+import unittest
+
+
+# In[2]:
+
+
+class TestIsGoodResponse(unittest.TestCase):
+    pass
+
+
+# In[4]:
+
+
+class TestSimpleGet(unittest.TestCase):
+    pass
+
+
+# In[5]:
+
+
+if __name__== '__main__':
+    unittest.main()
+
